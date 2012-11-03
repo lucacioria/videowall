1e4286adba4ee2315eb7e946b28dc1133eac2122
luca cioria <vircheck@gmail.com>
2012-11-03 01:20:20 -0500

    ddd

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,8 +4,13 @@ class VideosController < ApplicationController
 
   def index
     if user_signed_in? then
+      ### videos I like
       videos_i_like = get_videos_i_like()
-      update_videos_in_database(videos_i_like)
+      update_videos_i_like_in_database(videos_i_like)
+      ### videos I posted
+      videos_i_posted = get_videos_i_posted()
+      update_videos_i_posted_in_database(videos_i_posted)
+      ###
       render json: current_user.videos
     else
       render json: []
@@ -19,12 +24,29 @@ class VideosController < ApplicationController
     rest.fql_query("SELECT url FROM url_like WHERE user_id = me() AND strpos(lower(url), 'http://www.youtube.com/') == 0")
   end
 
-  def update_videos_in_database(videos_i_like)
+  def get_videos_i_posted
+    rest = Koala::Facebook::API.new(current_user.authentications.first.token)
+    rest.fql_query("SELECT created_time, url FROM link WHERE owner = me() AND strpos(lower(url), 'http://www.youtube.com/') == 0")
+  end
+
+  def update_videos_i_like_in_database(videos_i_like)
     videos_i_like.each do |video|
       if Video.find(:first, :conditions  => "video_url = '#{video["url"]}'").nil? then
         v = current_user.videos.new
         v.video_url = video["url"]
         v.video_type = "facebook_like"
+        v.save
+      end
+    end
+  end
+
+  def update_videos_i_posted_in_database(videos_i_posted)
+    videos_i_posted.each do |video|
+      if Video.find(:first, :conditions  => "video_url = '#{video["url"]}'").nil? then
+        v = current_user.videos.new
+        v.video_url = video["url"]
+        v.video_type = "facebook_post"
+        v.action_date = Time.at(video["created_time"].to_i)
         v.save
       end
     end
