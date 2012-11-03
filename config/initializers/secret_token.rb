f88c85e244537ab53f569241dca4d5472564181e
luca cioria <vircheck@gmail.com>
2012-11-02 19:04:57 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videowall::Application.config.secret_token = 'e966f88f61813c979001e3ac9ccf78915de7fdf2f4e426a9183bc2657f0e6b0721cbdb3995bb349f69dd03e2d57c693df9d7930a9608d364e63ac052191ed3f5'
