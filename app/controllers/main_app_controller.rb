eb8292580ae32d1174de9dbc06d452a6da468132
luca cioria <vircheck@gmail.com>
2012-11-02 22:54:11 -0500

    ddd

--- app/controllers/main_app_controller.rb
+++ app/controllers/main_app_controller.rb
@@ -8,7 +8,7 @@ class MainAppController < ApplicationController
 		if user_signed_in? then
 			@rest = Koala::Facebook::API.new(current_user.authentications.first.token)
 			#@likes = @rest.fql_query("SELECT * FROM url_like WHERE user_id = me() AND strpos(lower(url), 'http://www.youtube.com/') == 0")#.map{|x|"<a href='" + x["url"] + "'>" + x["url"] + "</a><br>"}
-			@likes = @rest.fql_query("SELECT url FROM url_like WHERE user_id = me() AND strpos(lower(url), 'http://www.youtube.com/') == 0").map{|x|"<a href='" + x["url"] + "'>" + x["url"] + "</a><br>"}
+			@likes = @rest.fql_query("SELECT url FROM url_like WHERE user_id = me() AND strpos(lower(url), 'http://www.youtube.com/') == 0")
 		else
 			@likes = "NOT SIGNED IN"
 		end
